7a820c65cb1fe6de25508ebdeb8e203582a89ac6
y777yx <18370097423@163.com>
2020-09-01 09:03:09 +0800

    乐宜兴第一次提交

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+      <router-link to="/index">首页</router-link>
     </div>
     <router-view/>
   </div>
---
+++ src/views/Index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>这是首页</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+
+  components: {},
+
+  computed: {},
+
+  mounted() {},
+
+  methods: {}
+};
+</script>
+<style lang='scss' scoped></style>
